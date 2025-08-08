33b6775ccbf12059d756519eaacce98897d3aa70
Austin Kho <kori.susie@gmail.com>
2025-08-09 03:03:22 +0900

    custom skin update

--- app/custom/modules/board/skins/faq/board.faq.js
+++ app/custom/modules/board/skins/faq/board.faq.js
@@ -1,71 +1,23 @@
-// jQuery(function($) {
-// 	// delete the border for the last row
-// 	$('.board_list tr:last-child>td').css('border','0');
-// 	// hide last tag
-// 	$('.read_footer .tags span:last-child').hide();
-// 	// display/hide serach box
-// 	var bs = $('.board_search');
-// 	bs.hide().addClass('off');
-// 	$('.bsToggle').click(function() {
-// 		if(bs.hasClass('off')) {
-// 			bs.show().removeClass('off').find('.iText').focus();
-// 		} else {
-// 			bs.hide().addClass('off');
-// 		}
-// 	});
-// 	// user input text blur/focus/change
-// 	var iText = $('.item .iLabel').next('.iText');
-// 	$('.item .iLabel').css('position','absolute');
-// 	iText
-// 		.focus(function() {
-// 			$(this).prev('.iLabel').css('visibility','hidden');
-// 		})
-// 		.blur(function() {
-// 			if(!$(this).val()) {
-// 				$(this).prev('.iLabel').css('visibility','visible');
-// 			} else {
-// 				$(this).prev('.iLabel').css('visibility','hidden');
-// 			}
-// 		})
-// 		.change(function() {
-// 			if(!$(this).val()) {
-// 				$(this).prev('.iLabel').css('visibility','visible');
-// 			} else {
-// 				$(this).prev('.iLabel').css('visibility','hidden');
-// 			}
-// 		})
-// 		.blur();
-// 	// add class to the parent category
-// 	$('.cTab>li>ul>li.on_').parents('li:first').addClass('on');
-// 	// delete the margin-top for the first child of the ccomments
-// 	$('.feedback .xe_content>*:first-child').css('margin-top','0');
-// });
-//
-// // SNS post
-// (function($) {
-// 	$.fn.snspost = function(opts) {
-// 		var loc = '';
-// 		opts = $.extend({}, {type:'twitter', event:'click', content:''}, opts);
-// 		opts.content = encodeURIComponent(opts.content);
-// 		switch(opts.type) {
-// 			case 'facebook':
-// 				loc = 'http://www.facebook.com/share.php?t='+opts.content+'&u='+encodeURIComponent(opts.url||location.href);
-// 				break;
-// 			case 'delicious':
-// 				loc = 'http://www.delicious.com/save?v=5&noui&jump=close&url='+encodeURIComponent(opts.url||location.href)+'&title='+opts.content;
-// 				break;
-// 			case 'twitter':
-// 				loc = 'http://twitter.com/home?status='+opts.content;
-// 				break;
-// 		}
-// 		this.bind(opts.event, function() {
-// 			window.open(loc);
-// 			return false;
-// 		});
-// 	};
-// 	$.snspost = function(selectors, action) {
-// 		$.each(selectors, function(key,val) {
-// 			$(val).snspost( $.extend({}, action, {type:key}) );
-// 		});
-// 	};
-// })(jQuery);
+jQuery(function($) {
+  $('.faq_title').on('click', function(e) {
+    e.preventDefault();
+    var targetId = $(this).attr('href');
+    var $target = $(targetId);
+
+    if ($target.hasClass('in')) {
+      // 닫기: max-height 0으로 애니메이션
+      $target.css('max-height', '0');
+      $target.removeClass('in');
+    } else {
+      // 열기: 현재 열려있는 거 닫기
+      $('.panel-collapse.in').each(function() {
+        $(this).css('max-height', '0').removeClass('in');
+      });
+
+      // 열기: 실제 컨텐츠 높이 측정 후 max-height에 세팅
+      $target.addClass('in');
+      var scrollHeight = $target.prop('scrollHeight') + 'px';
+      $target.css('max-height', scrollHeight);
+    }
+  });
+});
