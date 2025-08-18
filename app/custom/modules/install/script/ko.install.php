49f60e2a9429a15c153d20e8995dea32272b68ed
Austin Kho <kori.susie@gmail.com>
2025-08-18 21:31:42 +0900

    install post update

--- app/custom/modules/install/script/ko.install.php
+++ app/custom/modules/install/script/ko.install.php
@@ -566,7 +566,8 @@
 		$obj->title = $title;
 		// blade.php 파일 경로 확인 (현재 스크립트 파일과 같은 경로)
 		$script_dir = dirname(__FILE__);
-		$blade_file = $script_dir . '/pages' . $module_id . '.blade.php';
+		$docs_dir = $sort === 'page' ? '/pages' : '/posts';
+		$blade_file = $script_dir . $docs_dir . $module_id . '.blade.php';
 		
 		// blade.php 파일이 있으면 해당 내용을 사용, 없으면 $content 사용
 		if (file_exists($blade_file)) {
@@ -599,7 +600,7 @@
 		return $document_srl;
 	}
 	
-	// 실제 문서 삽입 코드
+	// 초기 문서 생성 코드
 	$page_list = array(
 		array( // 1. terms 내용 삽입
 			'module_id' => 'terms',
@@ -629,152 +630,22 @@
 	
 	foreach ($page_list as $page) insertCustomDocument($page['module_id'], $page['title'], '', $logged_info);
 	
-	// 7. FAQ 게시판에 자주 묻는 질문들 삽입
-	$faq_questions = array(
+	// 초기 게시물 생성 코드
+	$post_list = array(
 		array(
-			'title' => '우리 조합의 조합원 자격 요건(기준)은 어떻게 되나요?',
-			'content' => '
-			<p><span style="font-size: 10pt;"><u>지역주택조합의 조합원 자격은 주택법 시행령 제21조에 규정이 되어 있습니다.</u></span></p>
-			<table border="0" cellpadding="0" cellspacing="0" class="__se_tbl" style="border-width: 1px 1px 0px 0px; border-top-style: solid; border-right-style: solid; border-top-color: rgb(204, 204, 204); border-right-color: rgb(204, 204, 204); border-image: initial; border-left-style: initial; border-left-color: initial; border-bottom-style: initial; border-bottom-color: initial;">
-				<tbody>
-					<tr>
-						<td style="border-width: 0px 0px 1px 1px; border-bottom-style: solid; border-left-style: solid; border-bottom-color: rgb(204, 204, 204); border-left-color: rgb(204, 204, 204); border-image: initial; border-top-style: initial; border-top-color: initial; border-right-style: initial; border-right-color: initial; background-color: rgb(255, 255, 255); padding: 10px;" width="1069">
-						<p>&nbsp;<span class="bl" style="font-family: Gulim, doutm, tahoma, sans-serif; font-size: 13.2px; text-indent: -30px; color: rgb(21, 21, 148); margin: 0px; padding: 0px; font-weight: bold;">제21조(조합원의 자격)</span><span style="color: rgb(99, 99, 99); font-family: Gulim, doutm, tahoma, sans-serif; font-size: 13.2px; text-indent: -30px;">&nbsp;①&nbsp;</span><a class="link sfon1" style="font-family: Gulim, doutm, tahoma, sans-serif; font-size: 13.2px; text-indent: -30px; color: rgb(8, 109, 255); margin: 0px; padding: 0px; text-decoration-line: underline;" title="팝업으로 이동">법</a><span style="color: rgb(68, 68, 68); font-family: Gulim, doutm, tahoma, sans-serif; font-size: 13.2px; text-indent: -30px;">&nbsp;</span><a class="link sfon2" style="font-family: Gulim, doutm, tahoma, sans-serif; font-size: 13.2px; text-indent: -30px; color: rgb(0, 0, 205); margin: 0px; padding: 0px; text-decoration-line: underline;" title="팝업으로 이동">제11조</a><span style="color: rgb(99, 99, 99); font-family: Gulim, doutm, tahoma, sans-serif; font-size: 13.2px; text-indent: -30px;">에 따른 주택조합의 조합원이 될 수 있는 사람은 다음 각 호의 구분에 따른 사람으로 한다. 다만, 조합원의 사망으로 그 지위를 상속받는 자는 다음 각 호의 요건에도 불구하고 조합원이 될 수 있다. &nbsp;</span><span class="sfon" style="font-family: Gulim, doutm, tahoma, sans-serif; font-size: 13.2px; text-indent: -30px; color: rgb(2, 79, 206); margin: 0px; padding: 0px;">&lt;개정 2019. 10. 22.&gt;</span>&nbsp;</p>
-						<p class="pty1_de2h" style="padding-left: 48px; text-indent: -15px; color: rgb(68, 68, 68); font-family: Gulim, doutm, tahoma, sans-serif; font-size: 13.2px;"><span style="color: rgb(99, 99, 99);">1.&nbsp;</span><a class="oneView" style="color: rgb(0, 90, 132); margin: 0px; padding: 0px;">지역주택조합 조합원</a><span style="color: rgb(99, 99, 99);">: 다음 각 목의 요건을 모두 갖춘 사람</span></p>
-						<p class="pty1_de3" style="padding-left: 65px; text-indent: -17px; color: rgb(68, 68, 68); font-family: Gulim, doutm, tahoma, sans-serif; font-size: 13.2px;"><span style="color: rgb(99, 99, 99);">가. 조합설립인가 신청일(해당 주택건설대지가&nbsp;</span><a class="link sfon1" style="color: rgb(8, 109, 255); margin: 0px; padding: 0px; text-decoration-line: underline;" title="팝업으로 이동">법</a>&nbsp;<a class="link sfon2" style="color: rgb(0, 0, 205); margin: 0px; padding: 0px; text-decoration-line: underline;" title="팝업으로 이동">제63조</a><span style="color: rgb(99, 99, 99);">에 따른 투기과열지구 안에 있는 경우에는 조합설립인가 신청일 1년 전의 날을 말한다. 이하 같다)부터 해당 조합주택의 입주 가능일까지 주택을 소유(주택의 유형, 입주자 선정방법 등을 고려하여&nbsp;</span><a class="link" style="color: rgb(0, 90, 132); margin: 0px; padding: 0px; text-decoration-line: underline;" title="팝업으로 이동">국토교통부령</a><span style="color: rgb(99, 99, 99);">으로 정하는 지위에 있는 경우를 포함한다. 이하 이 호에서 같다)하는지에 대하여 다음의 어느 하나에 해당할 것</span></p>
-						<p class="pty1_de3" style="padding-left: 65px; text-indent: -17px; color: rgb(68, 68, 68); font-family: Gulim, doutm, tahoma, sans-serif; font-size: 13.2px;"><span style="color: rgb(99, 99, 99);">　 1)&nbsp;</span><a class="link" style="color: rgb(0, 90, 132); margin: 0px; padding: 0px; text-decoration-line: underline;" title="팝업으로 이동">국토교통부령</a><span style="color: rgb(99, 99, 99);">으로 정하는 기준에 따라 세대주를 포함한 세대원[세대주와 동일한 세대별 주민등록표에 등재되어 있지 아니한 세대주의 배우자 및 그 배우자와 동일한 세대를 이루고 있는 사람을 포함한다. 이하 2)에서 같다] 전원이 주택을 소유하고 있지 아니한 세대의 세대주일 것</span></p>
-						<p class="pty1_de3" style="padding-left: 65px; text-indent: -17px; color: rgb(68, 68, 68); font-family: Gulim, doutm, tahoma, sans-serif; font-size: 13.2px;"><span style="color: rgb(99, 99, 99);">　 2)&nbsp;</span><a class="link" style="color: rgb(0, 90, 132); margin: 0px; padding: 0px; text-decoration-line: underline;" title="팝업으로 이동">국토교통부령</a><span style="color: rgb(99, 99, 99);">으로 정하는 기준에 따라 세대주를 포함한 세대원 중 1명에 한정하여 주거전용면적 85제곱미터 이하의 주택 1채를 소유한 세대의 세대주일 것</span></p>
-						<p class="pty1_de3" style="padding-left: 65px; text-indent: -17px; color: rgb(68, 68, 68); font-family: Gulim, doutm, tahoma, sans-serif; font-size: 13.2px;"><span style="color: rgb(99, 99, 99);">나. 조합설립인가 신청일 현재&nbsp;</span><a class="link sfon1" style="color: rgb(8, 109, 255); margin: 0px; padding: 0px; text-decoration-line: underline;" title="팝업으로 이동">법</a>&nbsp;<a class="link sfon2" style="color: rgb(0, 0, 205); margin: 0px; padding: 0px; text-decoration-line: underline;" title="팝업으로 이동">제2조</a><a class="link sfon4" style="color: rgb(0, 0, 105); margin: 0px; padding: 0px; text-decoration-line: underline;" title="팝업으로 이동">제11호</a><a class="link sfon5" style="color: rgb(95, 146, 160); margin: 0px; padding: 0px; text-decoration-line: underline;" title="팝업으로 이동">가목</a><span style="color: rgb(99, 99, 99);">의 구분에 따른 지역에 6개월 이상 계속하여 거주하여 온 사람일 것</span></p>
-						<p class="pty1_de3" style="padding-left: 65px; text-indent: -17px; color: rgb(68, 68, 68); font-family: Gulim, doutm, tahoma, sans-serif; font-size: 13.2px;"><span style="color: rgb(99, 99, 99);">다. 본인 또는 본인과 같은 세대별 주민등록표에 등재되어 있지 않은 배우자가 같은 또는 다른 지역주택조합의 조합원이거나 직장주택조합의 조합원이 아닐 것</span></p>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-			<p>&nbsp;</p>
-			<p>&nbsp;</p>
-			<p><span style="font-size: 10pt;"><u>우리 조합은 설립인가 신청 전이며, 투기과열지구에 해당하지 않으므로 조합원 자격 요건은 다음과 같습니다.</u></span></p>
-			<table _se2_tbl_template="4" border="0" cellpadding="0" cellspacing="0" class="__se_tbl" style="border: 1px solid rgb(199, 199, 199);">
-				<tbody>
-					<tr>
-						<td style="width: 134px; height: 18px; padding: 3px 4px 2px; color: rgb(102, 102, 102); border-right: 1px solid rgb(231, 231, 231); background-color: rgb(243, 243, 243);">
-						<p style="text-align: center; margin-left: 0px;">구분</p>
-						</td>
-						<td style="width: 234px; height: 18px; padding: 3px 4px 2px; color: rgb(102, 102, 102); border-right: 1px solid rgb(231, 231, 231); background-color: rgb(243, 243, 243);">
-						<p style="text-align: center; margin-left: 0px;">기간&nbsp;</p>
-						</td>
-						<td style="width: 697px; height: 18px; padding: 3px 4px 2px; color: rgb(102, 102, 102); border-right: 1px solid rgb(231, 231, 231); background-color: rgb(243, 243, 243);">
-						<p align="center" style="text-align: center; margin-left: 0px;">내용</p>
-						</td>
-					</tr>
-					<tr>
-						<td style="width: 134px; height: 18px; padding: 3px 4px 2px; border-top: 1px solid rgb(231, 231, 231); border-right: 1px solid rgb(231, 231, 231); color: rgb(102, 102, 102); background-color: rgb(255, 255, 255);">
-						<p style="text-align: center; margin-left: 0px;">거주요건</p>
-						</td>
-						<td style="width: 234px; height: 18px; padding: 3px 4px 2px; border-top: 1px solid rgb(231, 231, 231); border-right: 1px solid rgb(231, 231, 231); color: rgb(102, 102, 102); background-color: rgb(255, 255, 255);">
-						<p style="text-align: center; margin-left: 0px;">설립인가 신청일 전 6개월 간</p>
-						</td>
-						<td style="width: 697px; height: 18px; padding: 3px 4px 2px; border-top: 1px solid rgb(231, 231, 231); border-right: 1px solid rgb(231, 231, 231); color: rgb(102, 102, 102); background-color: rgb(255, 255, 255);">
-						<p style="margin-left: 40px;">해당 기간 동안 <span style="color: rgb(0, 117, 200);">서울특별시</span><span style="color: rgb(0, 117, 200); font-family: Gulim, doutm, tahoma, sans-serif; font-size: 13.2px; text-indent: -2px;">ㆍ</span><span style="color: rgb(0, 117, 200);">인천광역시 및 경기도</span> 거주</p>
-						</td>
-					</tr>
-					<tr>
-						<td style="width: 134px; height: 18px; padding: 3px 4px 2px; border-top: 1px solid rgb(231, 231, 231); border-right: 1px solid rgb(231, 231, 231); color: rgb(102, 102, 102); background-color: rgb(255, 255, 255);">
-						<p style="text-align: center; margin-left: 0px;">주택소유 요건</p>
-						</td>
-						<td colspan="1" rowspan="3" style="width: 234px; height: 41px; padding: 3px 4px 2px; border-top: 1px solid rgb(231, 231, 231); border-right: 1px solid rgb(231, 231, 231); color: rgb(102, 102, 102); background-color: rgb(255, 255, 255);">
-						<!--<p align="left" style="text-align: center; margin-left: 0px;"><b><span style="color: rgb(0, 117, 200);">2019. 07. 30.</span></b> - 입주가능일 까지</p>-->
-						<p align="left" style="text-align: center; margin-left: 0px;">설립인가 신청일 - 입주가능일 까지</p>
-						</td>
-						<td style="width: 697px; height: 18px; padding: 3px 4px 2px; border-top: 1px solid rgb(231, 231, 231); border-right: 1px solid rgb(231, 231, 231); color: rgb(102, 102, 102); background-color: rgb(255, 255, 255);">
-						<p style="margin-left: 40px;">해당 기간 동안 본인 및 배우자, 세대원 전원이 무주택 또는 전용면적 85제곱미터 이하의 주택(분양권 포함) 1채를 소유</p>
-						</td>
-					</tr>
-					<tr>
-						<td style="width: 134px; height: 18px; padding: 3px 4px 2px; border-top: 1px solid rgb(231, 231, 231); border-right: 1px solid rgb(231, 231, 231); color: rgb(102, 102, 102); background-color: rgb(255, 255, 255);">
-						<p style="text-align: center; margin-left: 0px;">세대주 요건</p>
-						</td>
-						<td style="width: 697px; height: 18px; padding: 3px 4px 2px; border-top: 1px solid rgb(231, 231, 231); border-right: 1px solid rgb(231, 231, 231); color: rgb(102, 102, 102); background-color: rgb(255, 255, 255);">
-						<p style="margin-left: 40px;">해당 기간 동안 연속적으로 본인이 세대주일 것</p>
-						</td>
-					</tr>
-					<tr>
-						<td colspan="1" rowspan="1" style="width: 5px; height: 5px; padding: 3px 4px 2px; border-top: 1px solid rgb(231, 231, 231); border-right: 1px solid rgb(231, 231, 231); color: rgb(102, 102, 102); background-color: rgb(255, 255, 255);">
-						<p style="margin-left: 0px; text-align: center;">중복가입금지</p>
-						</td>
-						<td colspan="1" rowspan="1" style="width: 342px; height: 5px; padding: 3px 4px 2px; border-top: 1px solid rgb(231, 231, 231); border-right: 1px solid rgb(231, 231, 231); color: rgb(102, 102, 102); background-color: rgb(255, 255, 255);">
-						<p style="margin-left: 40px;">해당 기간 동안 본인 및 배우자가 다른 지역주택조합 또는 직장주택조합의 조합원이 아닐 것</p>
-						</td>
-					</tr>
-				</tbody>
-			</table>'
+			'module_id' => 'notice',
+			'title' => '[안내] 조합 홈페이지 오픈 안내'
 		),
-	);
-	
-	// FAQ 게시글들 삽입
-	foreach ($faq_questions as $faq) insertCustomDocument('faq', $faq['title'], $faq['content'], $logged_info, 'board');
-	
-	// 8. 공지사항 게시판에 공지사항 삽입
-	$notices = array(
 		array(
-			'title' => '[안내] 조합 홈페이지 오픈 안내',
-			'content' => '
-			<h3><u><b>조합 공식 홈페이지 오픈 안내</b></u></h3>
-			<p>&nbsp;</p>
-			<p>안녕하세요, 조합원 여러분.</p>
-			<p>조합의 사업 진행 상황과 각종 소식을 보다&nbsp;<b>신속하고 투명하게</b>&nbsp;전달해 드리기 위해&nbsp;<b>조합 공식 홈페이지</b>를 새롭게 오픈하였습니다.</p>
-			<p>&nbsp;</p>
-			<table border="1" cellpadding="30" cellspacing="0" style="width:740px;">
-				<tbody>
-					<tr>
-						<td style="padding: 20px; background: #fffff2;">
-						<h3><b>주요 기능 안내</b></h3>
-						<p>&nbsp;</p>
-						<ul>
-							<li>
-							<p><b>사업 진행 현황 실시간 업데이트</b></p>
-							<p>진행 중인 사업의 최신 소식을 빠르게 확인하실 수 있습니다.</p>
-							</li>
-							<li>
-							<p><b>공지사항 및 안내사항 게시</b></p>
-							<p>중요한 일정과 변경 사항을 놓치지 않도록 안내드립니다.</p>
-							</li>
-							<li>
-							<p><b>법정 공개 자료 열람</b></p>
-							<p>주택법 등 관련 법규에 따라 공개해야 하는 자료를 열람하실 수 있습니다.</p>
-							</li>
-							<li>
-							<p><b>커뮤니티 게시판 운영</b></p>
-							<p>조합원 간 자유로운 의견 교환이 가능한 자유게시판과 질문게시판을 제공합니다.</p>
-							</li>
-						</ul>
-						<p>&nbsp;</p>
-						<p>&nbsp;</p>
-						<h3><b>조합원 인증 안내</b></h3>
-						<p>&nbsp;</p>
-						<ul>
-							<li>
-							<p><b>조합원 전용 콘텐츠</b>는 인증된 조합원만 열람할 수 있습니다.</p>
-							</li>
-							<li>
-							<p>인증을 원하시는 경우&nbsp;<b>&lsquo;조합원 인증 요청&rsquo; 게시판</b>에서 안내 절차에 따라 신청해 주세요.</p>
-							</li>
-							<li>
-							<p>인증이 완료되면 모든 자료를 열람하고, 게시판에 자유롭게 글을 작성하실 수 있습니다.</p>
-							</li>
-						</ul>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-			<p>&nbsp;</p>
-			<p>조합 공식 홈페이지가 조합원 여러분과의 소통 창구가 되어,</p>
-			<p>사업 진행의 모든 과정을 투명하고 편리하게 공유할 수 있도록 노력하겠습니다.</p>
-			<p>많은 방문과 이용 부탁드립니다.<a href="https://www.facebook.com/"><i></i></a></p>'
+			'module_id' => 'faq',
+			'title' => '우리 조합의 조합원 자격 요건(기준)은 어떻게 되나요?'
 		)
 	);
 	
-	// 공지사항 게시글들 삽입
-	foreach ($notices as $notice) insertCustomDocument('notice', $notice['title'], $notice['content'], $logged_info, 'board', 'Y');
+	foreach ($post_list as $post) {
+		$is_notice = $post['module_id'] === 'faq' ? 'N' : 'Y';
+		insertCustomDocument($post['module_id'], $post['title'], '', $logged_info, 'board', $is_notice);
+	}
 	
 	// ========== 게시판 권한 설정 함수 ==========
 	function setBoardPermissions($module_id, $permissions = array()): bool
